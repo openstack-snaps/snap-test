0d93a8e3d7ed5a311e03bdfb655c10bf0666f8f5
James Page <james.page@ubuntu.com>
2016-11-24 11:19:38 +0000

    Make database script idempotent

--- scripts/database.sh
+++ scripts/database.sh
@@ -3,22 +3,22 @@
 set -ex
 
 sudo mysql -u root << EOF
-CREATE DATABASE keystone;
+CREATE DATABASE IF NOT EXISTS keystone;
 GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' \
   IDENTIFIED BY 'keystone';
-CREATE DATABASE nova;
-CREATE DATABASE nova_api;
+CREATE DATABASE IF NOT EXISTS nova;
+CREATE DATABASE IF NOT EXISTS nova_api;
 GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' \
   IDENTIFIED BY 'nova';
 GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'localhost' \
   IDENTIFIED BY 'nova';
-CREATE DATABASE neutron;
+CREATE DATABASE IF NOT EXISTS neutron;
 GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'localhost' \
   IDENTIFIED BY 'neutron';
-CREATE DATABASE glance;
+CREATE DATABASE IF NOT EXISTS glance;
 GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'localhost' \
   IDENTIFIED BY 'glance';
-CREATE DATABASE cinder;
+CREATE DATABASE IF NOT EXISTS cinder;
 GRANT ALL PRIVILEGES ON cinder.* TO 'cinder'@'localhost' \
   IDENTIFIED BY 'cinder';
 EOF

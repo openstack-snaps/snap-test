cff7b1bc65c1c7bd7a836a8a32d75c7eede2e268
Corey Bryant <corey.bryant@canonical.com>
2017-07-11 19:45:01 +0000

    Add nova-hypervisor interface connection commands
    These are actually still needed if the snap is not installed from the
    snap store. Only manually connect the interfaces if they're not already
    connected.

--- scripts/nova-hypervisor.sh
+++ scripts/nova-hypervisor.sh
@@ -8,6 +8,15 @@ snap list | grep -q "^nova-hypervisor\s" || {
     sudo snap install --edge nova-hypervisor
 }
 
+# Manually connect interfaces if snap isn't installed from snap store.
+# Otherwise, snap store automatically connects these interfaces.
+interfaces=( firewall-control hardware-observe libvirt network-control
+             network-observe openvswitch process-control system-observe )
+for interface in "${interfaces[@]}"; do
+    snap interfaces -i ${interface} nova-hypervisor | grep nova-hypervisor:${interface} && \
+    sudo snap connect nova-hypervisor:${interface} core:${interface}
+done
+
 while sudo [ ! -d /var/snap/nova-hypervisor/common/etc/neutron/ ]; do sleep 0.1; done;
 while sudo [ ! -d /var/snap/nova-hypervisor/common/etc/nova/ ]; do sleep 0.1; done;
 sudo cp -r $BASE_DIR/etc/snap-nova-hypervisor/* /var/snap/nova-hypervisor/common/etc/
